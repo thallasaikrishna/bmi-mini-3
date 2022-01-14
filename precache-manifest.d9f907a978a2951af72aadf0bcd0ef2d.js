46b4184765ce1fdb63c888f9a53f08ec1b821507
thallasaikrishna <thallasaikrishna@gmail.com>
2022-01-15 00:01:44 +0530

    Updates

---
+++ precache-manifest.d9f907a978a2951af72aadf0bcd0ef2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e2590c99116aa4dc7704784e466c223",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "f3adbcc9263db2f9907e",
+    "url": "/my-app/static/css/main.1f925377.chunk.css"
+  },
+  {
+    "revision": "8b8d5a007a02500c6c63",
+    "url": "/my-app/static/js/2.624f555d.chunk.js"
+  },
+  {
+    "revision": "f589bf591c94edd204e3",
+    "url": "/my-app/static/js/3.d439ba6f.chunk.js"
+  },
+  {
+    "revision": "f3adbcc9263db2f9907e",
+    "url": "/my-app/static/js/main.b981a94a.chunk.js"
+  },
+  {
+    "revision": "bdbff44f4040b27f640a",
+    "url": "/my-app/static/js/runtime~main.fdf3adfd.js"
+  }
+]);
